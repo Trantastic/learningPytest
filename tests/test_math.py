b2b5d132d595ddca3f6250f18c3779cb0d31a1dc
Yvette Tran <yvettetran@gmail.com>
2023-10-20 10:52:29 -0400

    my first test

---
+++ tests/test_math.py
@@ -0,0 +1,6 @@
+#In pytest, any tests with the test_ naming will be executed as test cases
+def test_one_plus_one():
+    #assert statement is native to python
+    assert 1 + 1 == 2
+
+#To run test on commandline, python3 -m pytest
