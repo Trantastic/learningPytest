5923c07ba0b59f1b8d87eaa84404d3a5d2410e3e
Yvette Tran <yvettetran@gmail.com>
2023-10-22 15:10:12 -0400

    creating test case with parameters

--- tests/test_math.py
+++ tests/test_math.py
@@ -20,7 +20,24 @@ def test_one_plus_two():
     c = 3
     assert a + b == c
 
+#Test case with an exception
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
     assert 'division by zero' in str(e.value)
+
+#Parameterized test case
+products = [
+    (2, 3, 6),
+    (1, 99, 99),
+    (0, 99, 0),
+    (3, -4, -12),
+    (-5, -5, 25),
+    (2.5, 6.7, 16.75)
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
+
+
